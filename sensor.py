acd737ffa95f249a8ee091ae708dea9b50d71a32
neurax6 <waldg3ist@hotmail.com>
2015-07-07 01:33:45 -0400

    fdsa

--- sensor.py
+++ sensor.py
@@ -18,7 +18,8 @@ class Loader(object):
     def delsensor(self, p_linkid):
         i = 0
         while i < len(self.__sensorlist):
-            if p_linkid in self.__sensorlist[i].getlinkid():
+            test = self.__sensorlist[i]
+            if p_linkid in test.getlinkid():
                 self.__sensorlist.remove(self.__sensorlist[i])
         i += 1
 

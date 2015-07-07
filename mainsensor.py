9a7c27aa19fa221e701ad2371de6cedee7c3b017
neurax6 <waldg3ist@hotmail.com>
2015-07-07 02:03:35 -0400

    fdsa

--- mainsensor.py
+++ mainsensor.py
@@ -6,13 +6,14 @@ from sensor import *
 def main():
     sensor = Dht(p_link=666, p_link_id=667)
     sensor2 = Dht(p_link=667, p_link_id=666)
+    sensor3 = Dht(p_link=677, p_link_id=777)
     config = Loader()
     config.addsensor(sensor)
     config.addsensor(sensor2)
     print(config.listsensor())
-
+    config.delsensor(666)
     print(config.listsensor())
-
-
+    config.addsensor(sensor3)
+    print(config.listsensor())
 if __name__ == "__main__":
     main()
